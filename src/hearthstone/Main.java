4f091b06801fe238e9db6bf1421259699ac23bfb
jakegsy <siyuangoh@gmail.com>
2016-11-24 07:56:13 +0800

    Initial commit

---
+++ src/hearthstone/Main.java
@@ -0,0 +1,42 @@
+package hearthstone;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application implements EventHandler<ActionEvent>{
+
+    Stage window;
+    Button button;
+    Scene scene1, scene2;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        this.button = new Button();
+        button.setText("Click Me");
+
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource()==button){
+            System.out.println("Ok");
+        }
+    }
+}
